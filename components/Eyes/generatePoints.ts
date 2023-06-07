68e1b49e76af577ebd52afbfc11a6c851173281d
Benji Grant <benjamin.grantGRA0007@gmail.com>
2023-06-07 15:10:47 +1000

    Limit the amount of eyes that can be generated

--- components/Eyes/generatePoints.ts
+++ components/Eyes/generatePoints.ts
@@ -1,18 +1,25 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore No types
 import PoissonDiskSampling from 'poisson-disk-sampling'
 
 export const generatePoints = (height: number, width: number, size: number) => {
   const sample = new PoissonDiskSampling({
-    shape: [height, width],
+    shape: [Math.min(height, 2000), Math.min(width, 2000)],
     minDistance: size,
     maxDistance: size + (size * .5),
     tries: 20,
   })
 
-  const points: [number, number][] = sample.fill()
+  // Area to avoid in the centre
+  const avoid = {
+    y: sample.shape[0] / 2,
+    x: sample.shape[1] / 2,
+    r: size * 1.5, // radius
+  }
+
+  const points = sample.fill()
 
   return points
-    .map(([y, x]) => [(y / height) * 100, (x / width) * 100] as [number, number])
-    .filter(([y, x]) => !(y > 30 && y < 70 && x > 40 && x < 60))
+    // Remove points from the avoidance area
+    .filter(([y, x]) => Math.pow(avoid.y - y, 2) + Math.pow(avoid.x - x, 2) > Math.pow(avoid.r, 2))
+    // Convert to percentages
+    .map(([y, x]) => [(y / sample.shape[0]) * 100, (x / sample.shape[1]) * 100] as [number, number])
 }
