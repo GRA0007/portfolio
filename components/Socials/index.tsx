634280da20c230894aa222bbd68b3a8e13e15e1e
Benji Grant <benjamin.grantGRA0007@gmail.com>
2024-12-27 15:21:16 +1100

    Fix file capitalization

--- components/Socials/index.tsx
+++ components/Socials/index.tsx
@@ -1,4 +1,4 @@
-import { SOCIAL_LINKS } from './socials'
+import { SOCIAL_LINKS } from './socialLinks'
 
 export const Socials = () => (
   <ul className="flex items-center justify-center gap-6" aria-label="Social links">
