e8ae437b39a60e685c4bf8f436748cd9445c4080
Benji Grant <benjamin.grantGRA0007@gmail.com>
2025-11-07 13:27:38 +1100

    Add margin to the bottom of the home page

--- recipes/app/page.tsx
+++ recipes/app/page.tsx
@@ -8,7 +8,7 @@ const Home = async () => {
   const recipes = await fetchRecipes()
 
   return (
-    <div className="mx-auto w-full max-w-7xl grid-cols-[1fr_auto] grid-rows-[auto_1fr] gap-x-10 px-6 [grid-template-areas:'nav_search'_'results_search'] md:grid md:px-16">
+    <div className="mx-auto mb-16 w-full max-w-7xl grid-cols-[1fr_auto] grid-rows-[auto_1fr] gap-x-10 px-6 [grid-template-areas:'nav_search'_'results_search'] md:grid md:px-16">
       <nav className="flex pt-6 [grid-area:nav] sm:pt-8 md:py-12">
         <Logo isHeading />
       </nav>
